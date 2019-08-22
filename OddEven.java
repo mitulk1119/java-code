15796557cde88999bb93e6a0ab9a36f5b3393143
masudhabib <masudhabib@users.noreply.github.com>
2019-08-22 10:21:43 -0700

    might consider bit shifting

--- OddEven.java
+++ OddEven.java
@@ -5,7 +5,7 @@ public class OddEven {
 		Scanner oe = new Scanner(System.in);
 		System.out.println("PLEase enter the number");
 		int i = oe.nextInt();
-		if (i % 2 == 0) {
+		if (i % 2 == 0) {				//TO CONSIDER: compare performance using bit shifting method to determine if even or odd
 			System.out.println("It is Even Number");
 		} else {
 			System.out.println("IT is Odd Number");
