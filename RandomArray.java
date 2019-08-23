93803226aca6b54cea7f4fd5ba9634d89f2b4926
mitulk1119 <54382432+mitulk1119@users.noreply.github.com>
2019-08-23 11:46:12 -0400

    Add files via upload

--- RandomArray.java
+++ RandomArray.java
@@ -2,15 +2,21 @@ import java.util.Random;
 
 public class RandomArray {
 
-	public static void main(String[] args) {
+		int computeRandomArray(int i) {
 		Random r = new Random();
 		int sum = 0;
-		for (int i = 0; i < 100; i++) {
+		for ( i = 0; i < 100; i++) {
 			int randomint = r.nextInt(100);
 			System.out.print(randomint + " ,");
-			sum = sum + randomint;
+			 sum = sum + randomint;
 		}
-		System.out.println("\nSum = " + sum);
+		return sum;
+		}
+		public static void main(String[] args) {
+			int i = 0;
+			RandomArray obj = new RandomArray();
+			int RandomArray= obj.computeRandomArray(i);
+		System.out.println("\nSum = " + RandomArray);
 
 	}
 
--- RandomOdd.java
+++ RandomOdd.java
@@ -1,16 +1,23 @@
 import java.util.Random;
 
 public class RandomOdd {
-	public static void main(String[] args) {
+	
+	int computeRandomOdd(int i) {
 		Random r = new Random();
 		int sum = 0;
-		for (int i = 1; i <= 100; i++) {
+		for (i = 1; i <= 100; i++) {
 			int randomint = r.nextInt(100);
 			System.out.print(randomint + " ,");
 			if (randomint % 2 != 0) {
 				sum = randomint + sum;
 			}
-		}
-		System.out.println("\nThe Sum Of 10 Odd Numbers are:" + sum);
+		}return sum;
+	}
+	public static void main(String[] args) {
+		
+		int i = 0;
+		RandomOdd obj = new RandomOdd();
+		int RandomArray= obj.computeRandomOdd(i);
+		System.out.println("\nThe Sum Of 100 Odd Numbers are:" + RandomArray);
 	}
 }
